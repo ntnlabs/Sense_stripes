a02fccaab25eef21c347bfbf4e2a8a27d9bb290d
NTN Labs <ntn.labs@gmail.com>
2018-09-03 00:10:08 +0200

    Update sense.py

--- sense.py
+++ sense.py
@@ -7,38 +7,20 @@ sense.set_rotation(0)
 sense.low_light = True
 sense.clear()
 
-# define many colors
+# define some colors
 b = (0,0,0)
 r1 = (255,0,0)
 r2 = (192,0,0)
 r3 = (129,0,0)
-r4 = (66,0,0)
 g1 = (0,255,0)
 g2 = (0,192,0)
 g3 = (0,129,0)
-g4 = (0,66,0)
 b1 = (0,0,255)
 b2 = (0,0,192)
 b3 = (0,0,129)
-b4 = (0,0,66)
-rb1 = (255,0,255)
-rb2 = (192,0,192)
-rb3 = (129,0,129)
-rb4 = (66,0,66)
-gb1 = (0,255,255)
-gb2 = (0,192,192)
-gb3 = (0,129,129)
-gb4 = (0,66,66)
-rg1 = (255,255,0)
-rg2 = (192,192,0)
-rg3 = (129,129,0)
-rg4 = (66,66,0)
-rgb1 = (255,255,255)
-rgb2 = (192,192,192)
-rgb3 = (129,129,129)
-rgb4 = (66,66,66)
 
-# add startup colorful effect
+# LED startup
+# TODO: add startup colorful effect
 line0 = [b,b,b,b,b,b,b,b]
 line1 = [r2,r2,r2,r2,r2,r2,r2,r2]
 line2 = [g2,g2,g2,g2,g2,g2,g2,g2]
@@ -50,17 +32,16 @@ sleep(1)
 sense.clear()
 
 while True:
-
   # Take integer readings from all three sensors
   t = int(sense.get_temperature())
   p = int(sense.get_pressure())
   h = int(sense.get_humidity())
 
-  # Round the values to no decimal place
-  # t = round(t, 0)
-  # p = round(p, 0)
-  # h = round(h, 0)
-
+  # if temp is below 20, show weak red bar of minimal height (1 led)
+  # if temp is abowe 90, show bright red bar of full height (8 leds)
+  # if temp is between, show average red bar of height acording to the temp. 1 LED means 10 degrees celsius starting from 20 degrees celsius
+  # same for the humidity with blue color
+  # same for the pressure with green color and limiting values of 960 and 1030 hPa
   if t < 20:
    line1 = [b,b,b,b,b,b,b,r3]
   elif t > 90:
@@ -70,6 +51,15 @@ while True:
    if len(line1) < 8:
     line1 = line0[0:8-len(line1)]+line1
 
+  if p < 960:
+   line2 = [b,b,b,b,b,b,b,g3]
+  elif p > 1030:
+   line2 = [g1,g1,g1,g1,g1,g1,g1,g1]
+  else:
+   line2 = [g2]*int((p-960)/10)
+   if len(line2) < 8:
+    line2 = line0[0:8-len(line2)]+line2
+    
   if h < 20:
    line3 = [b,b,b,b,b,b,b,b3]
   elif h > 90:
@@ -78,15 +68,13 @@ while True:
    line3 = [b2]*int((h-10)/10)
    if len(line3) < 8:
     line3 = line0[0:8-len(line3)]+line3
-
+    
+  # just to see if it's doing what it is supposed to do
   # Create the message
   # str() converts the value to a string so it can be concatenated
   message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
 
-
-  # Display the scrolling message
-  # sense.show_message(message, scroll_speed=0.05)
   print(message)
   image = [line1+line1+line0+line2+line2+line0+line3+line3]
   sense.set_pixels(image[0])
-  sleep(10)
+  sleep(1)
