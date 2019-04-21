0e6deeeb195e5302783ab80748b8673767a45232
NTN Labs <ntn.labs@gmail.com>
2019-04-21 21:05:44 +0200

    Added rsyslog facility
    Old sqlite code commented out for easier understanding.

--- sense.py
+++ sense.py
@@ -1,11 +1,25 @@
 from sense_hat import SenseHat
 from time import sleep
+# import sqlite3
+from datetime import datetime
+import logging
+import logging.handlers
 
 # setup the enviroment
 sense = SenseHat()
-sense.set_rotation(0)
+sense.set_rotation(270)
 sense.low_light = True
 sense.clear()
+
+## prepare database
+#conn = sqlite3.connect('/home/pi/Database/sensor_data.db')
+#c = conn.cursor()
+
+# prepare syslog logger
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler(address = ('x.x.x.x',514))
+my_logger.addHandler(handler)
 
 # define some colors
 b = (0,0,0)
@@ -39,9 +53,10 @@ while True:
 
   # if temp is below 20, show weak red bar of minimal height (1 led)
   # if temp is abowe 90, show bright red bar of full height (8 leds)
-  # if temp is between, show average red bar of height acording to the temp. 1 LED means 10 degrees celsius starting from 20 degrees celsius
+    # if temp is between, show average red bar of height acording to the temp. 1 LED means 10 degrees celsius starting from 20 degrees celsius
   # same for the humidity with blue color
   # same for the pressure with green color and limiting values of 960 and 1030 hPa
+
   if t < 20:
    line1 = [b,b,b,b,b,b,b,r3]
   elif t > 90:
@@ -59,7 +74,7 @@ while True:
    line2 = [g2]*int((p-960)/10)
    if len(line2) < 8:
     line2 = line0[0:8-len(line2)]+line2
-    
+
   if h < 20:
    line3 = [b,b,b,b,b,b,b,b3]
   elif h > 90:
@@ -68,12 +83,20 @@ while True:
    line3 = [b2]*int((h-10)/10)
    if len(line3) < 8:
     line3 = line0[0:8-len(line3)]+line3
-    
+
   # just to see if it's doing what it is supposed to do
-  # message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
+  message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
 
   print(message)
   image = [line1+line1+line0+line2+line2+line0+line3+line3]
   sense.set_pixels(image[0])
-  # for my aplication a measurement once a minute is enough
-  sleep(60)
+
+  curr_datetime = datetime.now()
+  my_date = str(curr_datetime.strftime("%d.%m.%Y"))
+  my_time = str(curr_datetime.strftime("%H:%M:%S"))
+#  c.execute("INSERT INTO HAT_table VALUES (?,?,?,?,?)",(my_date,my_time,t,h,p))
+#  conn.commit()
+
+# go for syslog :)
+  my_logger.info(my_date+" : "+my_time+" temperature/humidity/pressure "+str(t)+" "+str(h)+" "+str(p))
+  sleep(59)
